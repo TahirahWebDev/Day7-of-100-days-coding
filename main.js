10488441c1b3455f246bb950b8ab8eface7bd025
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-07-06 01:16:55 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+//  Question 19
+let guests = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
+console.log(`I am inviting ${guests.length} people to dinner.`);
+//  Question 20
+let countries = ["Japan", "Canada", "New Zealand", "Iceland", "Switzerland"];
+console.log("Countries I'd like to visit:", countries);
+//  Question 21
+let books = {
+    title: "The Hobbit",
+    author: "J.R.R. Tolken",
+    year: 1973
+};
+console.log(`Book Info ${books.title} by ${books.author}, published in ${books.year}`);
